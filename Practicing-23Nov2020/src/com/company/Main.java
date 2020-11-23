89f098e11f927bc1edb41ba230edf1e611b4a27f
mfaisalghozi <mrafgaming@gmail.com>
2020-11-23 18:29:54 +0700

    done today

---
+++ Practicing-23Nov2020/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public Main(){
+        for(int i=0;i<10;i++){
+            System.out.println("Hello");
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
